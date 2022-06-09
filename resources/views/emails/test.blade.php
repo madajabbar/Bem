d7ea72c75063c34fb3108f6e36fe8b351ecc6d58
madajabbar <madajabbar22@gmail.com>
2022-06-09 22:51:29 +0700

    fixxx

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,8 @@
+<h3>Halo, {{ $nama }} !</h3>
+<p>Terimakasih telah menghubungi kami, Balasan dari pertanyaan anda adalah : <br>
+     {{ $isi }}
+</p>
+
+<p>Jika ada pertanyaan dapat anda ajukan lewat media sosial kami</p>
+<p>Dengan Hormat,</p>
+<p>Admin Bem Untan</p>
