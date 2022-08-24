d2f713f4cbcbabe4b535a54b3bfd887a915a19ff
madajabbar <madajabbar22@gmail.com>
2022-08-24 12:38:10 +0700

    fix di

--- resources/views/frontend/profil/struktur/index.blade.php
+++ resources/views/frontend/profil/struktur/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="h-1 mx gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
             </div>
             @forelse ($menteri->sortBy('jabatan') as $data)
-
+                @if($data->jabatan == 'Presiden Mahasiswa' || $data->jabatan == 'Wakil Presiden Mahasiswa')
                 <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
                     @forelse ($data->gambar as $gambar)
                     @if ($loop->last)
@@ -24,7 +24,7 @@
                     <div class="px-6 py-4">
                         <div class="font-bold text-xl mb-2">{{ $data->nama }}</div>
                         <p class="text-gray-700 text-base">
-                            {{$data->jabatan}} di {{$data->struktur->nama}}
+                            {{$data->jabatan}}
                         </p>
                     </div>
                     <div class="px-6 pt-4 pb-2">
@@ -36,6 +36,32 @@
                             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{$data->struktur->nama}}</span>
                     </div>
                 </div>
+                @else
+                <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+                    @forelse ($data->gambar as $gambar)
+                    @if ($loop->last)
+                    <img class="w-full" src="{{ asset('storage/' . $gambar->link) }}"
+                        alt="Sunset in the mountains">
+                    @endif
+                    @empty
+                        <img class="w-full" src="https://picsum.photos/200" alt="Sunset in the mountains">
+                    @endforelse
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">{{ $data->nama }}</div>
+                        <p class="text-gray-700 text-base">
+                            {{$data->jabatan}} {{$data->struktur->nama}}
+                        </p>
+                    </div>
+                    <div class="px-6 pt-4 pb-2">
+                        <span
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{$data->nim}}</span>
+                        <span
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{$data->jabatan}}</span>
+                        <span
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{$data->struktur->nama}}</span>
+                    </div>
+                </div>
+                @endif
                 @empty
             @endforelse
             @forelse ($anggota->sortBy('jabatan') as $data)
